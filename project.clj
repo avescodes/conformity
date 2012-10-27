3a342288ddb61c33eaedb8a56e502b57a73d1bb6
Ryan Neufeld <ryan@ryanneufeld.ca>
2012-10-26 20:04:05 -0400

    Version bump to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject conformity "0.0.1"
+(defproject conformity "0.1.0"
   :description "(Mostly) idempotent datom transacting for Datomic.\n\nSpecial thanks to Stuart Halloway for the original idea, implementation and permission to take it and run."
   :url "http://github.com/rkneufeld/conformity"
   :license {:name "Eclipse Public License"
