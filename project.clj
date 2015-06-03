4e973c6520e23875e08f514759302f81c013ec45
Stephen C. Gilardi <scgilardi@gmail.com>
2015-06-03 14:50:01 +0000

    snapshot version for dev

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.rkn/conformity "0.3.4"
+(defproject io.rkn/conformity "0.3.5-SNAPSHOT"
   :description "(Mostly) idempotent datom transacting for Datomic.\n\nSpecial thanks to Stuart Halloway for the original idea, implementation and permission to take it and run."
   :url "http://github.com/rkneufeld/conformity"
   :license {:name "Eclipse Public License"
